d65a32724c081cf5811d0e5a4d95ad21ff9439eb
Sumanth G <sumanthgenz@gmail.com>
2021-01-15 14:33:19 -0800

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -158,6 +158,3 @@ def compute_accuracy(logits, ground_truth, top_k=1):
     correct = pred.eq(ground_truth.reshape(1, -1).expand_as(pred))
     correct_k = correct[:top_k].reshape(-1).float().sum(0)
     return correct_k.mul_(100.0 / batch_size)
-
-x = torch.zeros(1, 2, 3)
-print(x.size(-2))
